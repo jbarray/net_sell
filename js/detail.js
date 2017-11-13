9c80f3e02f19ca5399fcc0caf4673b354cd26623
jby <1614963384@qq.com>
2017-11-13 15:44:04 +0800

    增加js样式:产品数量的改变 修复bug:选择尺寸功能完成后,选择颜色功能失效

--- js/detail.js
+++ js/detail.js
@@ -3,7 +3,9 @@ var searchValue=document.getElementsByClassName('search_text');
 var searchBtn=document.getElementsByClassName('search_btn');
 var productColor=document.getElementById('color');
 var productSize=document.getElementById('size');
-// var arrLink=document.getElementsByName('color');
+var upNum=document.getElementsByClassName('up');
+var downNum=document.getElementsByClassName('down');
+var productNumber=document.getElementById('productNum');
 /*input输入框(如果输入手机,跳转至choice页面)*/
 function input() {
     if(searchValue[0].value==="手机"){
@@ -17,29 +19,26 @@ searchBtn[0].addEventListener('click',function(){
     input();
 });
 /*产品颜色的选择*/
-window.onload=colorChange;
+colorChange();
+sizeChange();
 function  colorChange(){
-    var arrLink=document.getElementsByName('color');
     if(document.getElementsByName('color')){
+        var arrLink=document.getElementsByName('color');
         for(var i=0;i<arrLink.length;i++){
-            // if(i=0){
-            //     arrLink[0].className='beChoice';
-            // }
-            // else{
                 arrLink[i].className='notChoice';
                 arrLink[0].className='beChoice';
                 arrLink[i].onclick=clickNum;
-            // }
+                console.log(arrLink[i]);
         }
     }
 }
 function clickNum() {
-    var arrLink=document.getElementsByName('color');
     var target = event.currentTarget;
     if (document.getElementsByName('color')) {
+        var arrLink=document.getElementsByName('color');
         for (var i = 0; i < arrLink.length; i++) {
             if (arrLink[i].className = 'beChoice') {
-                arrLink[i].className = 'notChoice'
+                arrLink[i].className = 'notChoice';
             }
         }
         target.className='beChoice';
@@ -48,24 +47,24 @@ function clickNum() {
     }
 }
 /*产品型号的选择*/
-window.onload=sizeChange;
+// window.onload=sizeChange;
 function  sizeChange(){
-    var arrLink=document.getElementsByName('size');
     if(document.getElementsByName('size')){
-        for(var i=0;i<arrLink.length;i++){
-            arrLink[i].className='notChoice';
-            arrLink[0].className='beChoice';
-            arrLink[i].onclick=clickSize;
+        var sizeLink=document.getElementsByName('size');
+        for(var i=0;i<sizeLink.length;i++){
+            sizeLink[i].className='notChoice';
+            sizeLink[0].className='beChoice';
+            sizeLink[i].onclick=clickSize;
         }
     }
 }
 function clickSize() {
-    var arrLink=document.getElementsByName('size');
     var target = event.currentTarget;
+    var sizeLink=document.getElementsByName('size');
     if (document.getElementsByName('size')) {
-        for (var i = 0; i < arrLink.length; i++) {
-            if (arrLink[i].className = 'beChoice') {
-                arrLink[i].className = 'notChoice'
+        for (var i = 0; i < sizeLink.length; i++) {
+            if (sizeLink[i].className = 'beChoice') {
+                sizeLink[i].className = 'notChoice';
             }
         }
         target.className='beChoice';
@@ -73,3 +72,13 @@ function clickSize() {
         productSize.innerHTML=target.innerHTML;
     }
 }
+/*数量的变化*/
+/*点击加号*/
+upNum[0].onclick=function(){
+    productNumber.value++;
+};
+downNum[0].onclick=function(){
+    if(productNumber.value>1){
+        productNumber.value--;
+    }
+};
